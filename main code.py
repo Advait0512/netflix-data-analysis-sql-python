89505d46d062ae1bb7d3e0676bb4696685fe85f8
Advait0512 <155306397+Advait0512@users.noreply.github.com>
2026-05-07 13:39:20 +0530

    Add files via upload

---
+++ main code.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import mysql.connector
+import numpy as np
+
+# Load dataset
+df = pd.read_csv("kaggle data set/netflix_titles.csv")
+
+# Replace NaN with None
+df = df.replace({np.nan: None})
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="admin123",
+    database="netflix_db"
+)
+
+cursor = conn.cursor()
+
+# Insert data
+for i, row in df.iterrows():
+    sql = """
+    INSERT INTO netflix VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+    cursor.execute(sql, tuple(row))
+
+conn.commit()
+
+print("Data inserted successfully!")
